a5f712272c4384baa46e9535623f2d49f45e6fe8
Devin Leaman <dleaman212@gmail.com>
2018-05-16 12:51:29 -0500

    Upload functional

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,37 @@ const isLoggedIn = (req, res, next) => {
   res.redirect('/')
 }
 
+const { join } = require('path')
+const { lstatSync, readdirSync } = require('fs')
+const imgsPath = '../tron/images'
+
+const isDirectory = src => lstatSync(src).isDirectory()
+const getDirectories = src => readdirSync(src).map(name => join(src, name)).filter(isDirectory)
+const getDirectoryNames = dirs => {
+  let names = []
+  dirs.forEach(dir => names.push(dir.substring(dir.lastIndexOf('/') + 1)))
+  return names
+}
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, join(imgsPath, req.body.folder))
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.originalname.match(/(\.png$|\.gif$|\.jpg$)/) === null) cb(null, false)
+    else if (req.headers['content-length'] > 8000000) cb(null, false)
+    else cb(null, true)
+  }
+})
+
 module.exports = (app, passport) => {
   app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -31,6 +62,15 @@ module.exports = (app, passport) => {
 
   app.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile.ejs', { user: req.user })
+  })
+
+  app.get('/upload', isLoggedIn, (req, res) => {
+    res.render('upload.ejs', { user: req.user, message: '', paths: getDirectoryNames(getDirectories(imgsPath)) })
+  })
+
+  app.post('/upload', upload.array('fileUpload'), (req, res) => {
+    if (req.files.length === 0) res.render('upload.ejs', { user: req.user, message: 'Failed upload, make sure the file is not too large and it is actually a png/jpg/gif.' })
+    else res.render('upload.ejs', { user: req.user, message: `${req.files.length} image(s) uploaded.`, paths: getDirectoryNames(getDirectories(imgsPath)) })
   })
 
   app.get('/logout', (req, res) => {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 80
 const mongoose = require('mongoose')
 const passport = require('passport')
 const flash = require('connect-flash')
@@ -9,6 +9,8 @@ const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const favicon = require('serve-favicon')
+const path = require('path')
 
 const configdb = require('./config/database')
 
@@ -20,6 +22,7 @@ require('./config/passport')(passport)
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(bodyParser())
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
 
 app.set('view engine', 'ejs')
 
@@ -30,5 +33,6 @@ app.use(flash())
 
 require('./app/routes')(app, passport)
 
-app.listen(port)
-console.log(`Magic happens on port ${port}`)
+app.listen(port, () => {
+  console.log(`Magic happens on port ${port}`)
+})
